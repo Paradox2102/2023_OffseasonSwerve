af1600cc3f370dedeb5262c1f8ece4bbbb567b43
cpaggron <christopher.periodictable.pi@gmail.com>
2023-07-13 05:15:44 -0700

    Cleaned Up Errors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 // import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Commands.ArcadeDrive;
+import frc.robot.commands.ArcadeDrive;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.MaxSwerveModule;
 
