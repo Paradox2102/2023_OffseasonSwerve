342ac8b13abf97aa6e0444b72114b9f022674865
cpaggron <christopher.periodictable.pi@gmail.com>
2023-10-17 16:58:14 -0700

    Changes

--- src/main/java/frc/robot/commands/OuttakeCommand.java
+++ src/main/java/frc/robot/commands/OuttakeCommand.java
@@ -8,9 +8,7 @@ import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.WristSubsystem;
 
 
 
